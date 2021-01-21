9dd6dd99f4fb784880666bb385c3098a06827291
#AlbAnO#(^_^) <58682823+Albanolazaj10@users.noreply.github.com>
2021-01-21 19:42:07 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,22 @@
+// const navSlide = () => {
+//   const burger = document.querySelector(".hamburger");
+//   const nav = document.querySelector(".navbar-nav");
+//   const navLinks = document.querySelectorAll(".navbar-nav li");
+
+//   burger.addEventListener("click", () => {
+//     nav.classList.toggle("navbar-active");
+
+//     navLinks.forEach((link, index) => {
+//       if (link.style.animation) {
+//         link.style.animation = "";
+//       } else {
+//         link.style.animation = `navLinksFade 0.5s ease forwards  ${
+//           index / 7 + 0.3
+//         }s`;
+//       }
+//     });
+//     burger.classList.toggle("toggle");
+//   });
+// };
+
+// navSlide();
